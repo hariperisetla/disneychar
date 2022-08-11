a00fa1f0abd9cad1c634112fa44abd75d3320596
hariperisetla <perisetla.sai@gmail.com>
2022-08-12 00:13:37 +0530

    navbar and char gallery added

--- layouts/Header.jsx
+++ layouts/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <React.Fragment>
+      <nav className="bg-gray-900 border-b border-b-slate-700 p-5 flex items-center">
+        <div>
+          <Link href="/" className="block ">
+            <h1 className="cursor-pointer text-3xl text-white">Disney Chars</h1>
+          </Link>
+        </div>
+
+        <div className="text-slate-400 ml-auto text-xl space-x-8 cursor-pointer">
+          <Link href="/" className="">
+            <span className="hover:text-white">Home</span>
+          </Link>
+          <Link href="about" className="hover:text-white">
+            <span className="hover:text-white">About</span>
+          </Link>
+          <Link href="characters" className="hover:text-white">
+            <span className="hover:text-white">Characters</span>
+          </Link>
+          <Link href="contact" className="hover:text-white">
+            <span className="hover:text-white">Contact</span>
+          </Link>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
 };
 
 export default Header;
